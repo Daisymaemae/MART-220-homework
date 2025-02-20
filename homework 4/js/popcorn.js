f96c961bed8255426cfd9cb069a8b069567e120d
unknown <136731242+Daisymaemae@users.noreply.github.com>
2025-02-19 19:28:34 -0700

    Create popcorn.js

---
+++ homework 4/js/popcorn.js
@@ -0,0 +1,16 @@
+
+class popcorn
+{
+   constructor(path)
+   {
+    this.path = path;
+    // need the image
+    this.myImage = loadImage(this.path);
+   }    
+   draw()
+   {
+       // image draw
+       image(this.myImage, 100,100);
+   }
+
+}
